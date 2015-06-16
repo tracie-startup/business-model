7450d1a70ba8a9a424daf336c2eb9464cba8a9bf
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-06-16 10:46:34 +0200

    Change to authenticated users only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method   = :postmark
   config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: 'business-model.herokuapp.com' }
